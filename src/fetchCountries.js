5b632386f4e3fc5d8b1be1f6b7e398e20b63c610
Aleksandra Bukina <87126210+PhoenixBA@users.noreply.github.com>
2022-01-21 12:39:13 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 
 async function fetchCountry(name) {
-    const response = await fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,anguages`);
+    const response = await fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`);
     if (!response.ok) {
         throw new Error(response.status);
     }
